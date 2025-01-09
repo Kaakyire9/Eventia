b73b4704a2119a83e482f7dd2f7b0a24957ca86a
(Sampson) Reginald <r.aduboffour@gmail.com>
2025-01-09 13:40:33 +0000

    Add events, users, and rsvp apps; configure secret key management

--- ems/settings.py
+++ ems/settings.py
@@ -20,7 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0$378tm+0naj4xzxelzffpai9+__d)$st56q!0p#w^o2=q&rq3'
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
+SECRET_KEY = os.environ.get("SECRET_KEY")
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +43,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'events',
+    'users',
+    'rsvp',
 ]
 
 MIDDLEWARE = [
